d3a476f56acaa3739f5462b2977b967f0d070236
Maxim Pavlov <pavlov.web@gmail.com>
2025-01-01 17:40:52 +0100

    Implemented controls

--- hal/esp32/app_hal.cpp
+++ hal/esp32/app_hal.cpp
@@ -15,7 +15,7 @@
 
 // Function definitions
 void epd_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
-void joystick_read(lv_indev_t * indev, lv_indev_data_t * data);
+void joystick_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);
 void IRAM_ATTR isr();
 
 #if LV_USE_LOG != 0
@@ -27,12 +27,15 @@ static void lv_log_print_g_cb(lv_log_level_t level, const char *buf)
 #endif
 
 #ifndef MY_DISP_HOR_RES
-    #define MY_DISP_HOR_RES    480
+    #define MY_DISP_HOR_RES 480
 #endif
 
 #ifndef MY_DISP_VER_RES
-    #define MY_DISP_VER_RES    800
+    #define MY_DISP_VER_RES 800
 #endif
+
+// #define LV_TICK_CUSTOM 1
+// #define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())
 
 // Display variables
 // Static or global buffer(s). The second buffer is optional
@@ -46,10 +49,16 @@ static lv_disp_t* disp;
 uint16_t chunkCounter = 0;
 
 // Input control
+static lv_indev_drv_t indev_drv;
+static lv_indev_t * indev_keypad;
+
 SwithInputHandler inputHandler(BT_INPUT_2, BT_INPUT_1, BT_INPUT_0);
 volatile bool isrPending = false; 
 volatile unsigned long isrStartedAt = 0;
 
+void tick_timer_callback(void *arg) {
+    lv_tick_inc(1); // Increment LVGL tick by 1 ms
+}
 
 void create_black_square(lv_obj_t * parent) {
     // Create a new object (basic rectangle object)
@@ -72,57 +81,56 @@ void create_black_square(lv_obj_t * parent) {
     lv_obj_add_style(rect, &style_black, 0);
 }
 
-// static void event_handler(lv_event_t * e) {
-//     Serial.println("Button Event!");
-//      create_black_square(lv_screen_active());
-//     // lv_event_code_t code = lv_event_get_code(e);
-//     // lv_obj_t * obj = lv_event_get_target(e);
-//     // if(code == LV_EVENT_CLICKED) {
-//     // }
-// }
+static void event_handler(lv_event_t * e) {
+    Serial.println("Button Event!");
+    create_black_square(lv_scr_act());
+    // lv_event_code_t code = lv_event_get_code(e);
+    // lv_obj_t * obj = lv_event_get_target(e);
+    // if(code == LV_EVENT_CLICKED) {
+    // }
+}
 
-// void lv_example_list_1(void)
-// {
+void lv_example_list_1(void)
+{
+    lv_group_t * widget_group = lv_group_create();
 
-//     lv_group_t * widget_group = lv_group_create();
+    /*Create a list*/
+    lv_obj_t * list1 = lv_list_create(lv_scr_act());
+    lv_obj_set_size(list1, 256, 512);
+    lv_obj_center(list1);
 
-//     /*Create a list*/
-//     lv_obj_t * list1 = lv_list_create(lv_screen_active());
-//     lv_obj_set_size(list1, 256, 512);
-//     lv_obj_center(list1);
+    /*Add buttons to the list*/
+    lv_list_add_text(list1, "File");
 
-//     /*Add buttons to the list*/
-//     lv_list_add_text(list1, "File");
+    lv_obj_t * btn1 = lv_list_add_btn(list1, LV_SYMBOL_FILE, "New");
+    lv_group_add_obj(widget_group, btn1);
+    lv_obj_add_state(btn1, LV_STATE_CHECKED);
+    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_CLICKED, NULL);
 
-//     lv_obj_t * btn1 = lv_list_add_button(list1, LV_SYMBOL_FILE, "New");
-//     lv_group_add_obj(widget_group, btn1);
-//     lv_obj_add_state(btn1, LV_STATE_CHECKED);
-//     lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_CLICKED, NULL);
+    lv_obj_t * btn2 = lv_list_add_btn(list1, LV_SYMBOL_DIRECTORY, "Open");
+    lv_group_add_obj(widget_group, btn2);
+    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_CLICKED, NULL);
 
-//     lv_obj_t * btn2 = lv_list_add_button(list1, LV_SYMBOL_DIRECTORY, "Open");
-//     lv_group_add_obj(widget_group, btn2);
-//     lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_CLICKED, NULL);
+    lv_obj_t * btn3 = lv_list_add_btn(list1, LV_SYMBOL_SAVE, "Save");
+    lv_group_add_obj(widget_group, btn3);
+    lv_obj_add_event_cb(btn3, event_handler, LV_EVENT_CLICKED, NULL);
 
-//     lv_obj_t * btn3 = lv_list_add_button(list1, LV_SYMBOL_SAVE, "Save");
-//     lv_group_add_obj(widget_group, btn3);
-//     lv_obj_add_event_cb(btn3, event_handler, LV_EVENT_CLICKED, NULL);
+	static lv_style_t style_font24;
+    lv_style_init(&style_font24);
 
-// 	static lv_style_t style_font24;
-//     lv_style_init(&style_font24);
+    // Set the text font size
+    lv_style_set_text_font(&style_font24, &lv_font_montserrat_24); 
 
-//     // Set the text font size
-//     lv_style_set_text_font(&style_font24, &lv_font_montserrat_24); 
+	lv_obj_add_style(list1, &style_font24, LV_PART_MAIN); // Apply to the main part
+	lv_obj_add_style(list1, &style_font24, LV_PART_ITEMS);
 
-// 	lv_obj_add_style(list1, &style_font24, LV_PART_MAIN); // Apply to the main part
-// 	lv_obj_add_style(list1, &style_font24, LV_PART_ITEMS);
-
-//     // Button control 
-//     lv_indev_set_group(lvInput, widget_group);
-// }
+    // Button control 
+    lv_indev_set_group(indev_keypad, widget_group);
+}
 
 void hal_setup(void)
 {
-
+    // ----- EPD INIT BEGIN -----
     // Set display pins
     pinMode(4, INPUT);   // BUSY
     pinMode(16, OUTPUT); // RES
@@ -141,6 +149,9 @@ void hal_setup(void)
     // EPD_DeepSleep();         // Enter the sleep mode and please do not delete it, otherwise it will reduce the lifespan of the screen.
     // delay(2000);             // Delay for 2s.
 
+    // ----- EPD INIT END -----
+    // ----- DISPLAY DRIVER INIT START -----
+
     // ------ Start display config ------
     lv_disp_draw_buf_init(&draw_buf_dsc_1, buf_1, NULL, MY_DISP_HOR_RES * 32);   /*Initialize the display buffer*/
     lv_disp_drv_init(&disp_drv);                    /*Basic initialization*/
@@ -158,16 +169,28 @@ void hal_setup(void)
 
     // Get pointer to the active display 
     disp = lv_disp_drv_register(&disp_drv);
+    // ----- DISPLAY DRIVER INIT END -----
+    // ----- INPUT DRIVER INIT START -----
 
-    // // Set the input function
-    // inputHandler.configure(isr, 100, 2500);
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type    = LV_INDEV_TYPE_KEYPAD;
+    indev_drv.read_cb = joystick_read;
+    indev_keypad      = lv_indev_drv_register(&indev_drv);
 
-    // lvInput = lv_indev_create();
-    // lv_indev_set_type(lvInput, LV_INDEV_TYPE_KEYPAD);
-    // lv_indev_set_read_cb(lvInput, joystick_read);
+    inputHandler.configure(isr, 100, 2500);
 
-    // lv_example_list_1();
-    create_black_square(lv_disp_get_scr_act(disp));
+    // Set up the tick timer
+    const esp_timer_create_args_t tick_timer_args = {
+        .callback = &tick_timer_callback,
+        .name = "lv_tick_timer"
+    };
+    esp_timer_handle_t tick_timer;
+    esp_timer_create(&tick_timer_args, &tick_timer);
+    esp_timer_start_periodic(tick_timer, 1000); // 1ms interval
+
+    // ----- INPUT DRIVER INIT END -----
+    // create_black_square(lv_disp_get_scr_act(disp));
+    lv_example_list_1();
 }
 
 void hal_loop(void)
@@ -285,11 +308,11 @@ void epd_flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t *
 // LV_KEY_HOME      = 2,   /*0x02, STX*/
 // LV_KEY_END       = 3,   /*0x03, ETX*/
 
-void joystick_read(lv_indev_t *indev, lv_indev_data_t *data)
+void joystick_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
 {
     static bool btnPressed = false;
     bool heldBit = false;
-
+    
     // Serial.println("Joystick read triggered");
     uint8_t switchInput = inputHandler.handleInput(isrPending, isrStartedAt);
     if (switchInput) {
