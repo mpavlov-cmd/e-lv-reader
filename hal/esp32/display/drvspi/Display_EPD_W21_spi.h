9235fd930e69f8f9e5054e325b3e2056d7b64cf7
Maxim Pavlov <pavlov.web@gmail.com>
2025-01-07 14:24:42 +0100

    Reforat code for driver

--- hal/esp32/display/drvspi/Display_EPD_W21_spi.h
+++ hal/esp32/display/drvspi/Display_EPD_W21_spi.h
@@ -2,22 +2,19 @@
 #define _DISPLAY_EPD_W21_SPI_
 #include "Arduino.h"
 
-//IO settings
-//SCK--GPIO23(SCLK)
-//SDIN---GPIO18(MOSI)
-#define isEPD_W21_BUSY digitalRead(4)      //BUSY
-#define EPD_W21_RST_0 digitalWrite(16,LOW)  //RES
+// IO settings
+// SCK--GPIO23(SCLK)
+// SDIN---GPIO18(MOSI)
+#define isEPD_W21_BUSY digitalRead(4)       // BUSY
+#define EPD_W21_RST_0 digitalWrite(16,LOW)  // RES
 #define EPD_W21_RST_1 digitalWrite(16,HIGH)
-#define EPD_W21_DC_0  digitalWrite(17,LOW)  //DC
+#define EPD_W21_DC_0  digitalWrite(17,LOW)  // DC
 #define EPD_W21_DC_1  digitalWrite(17,HIGH)
 #define EPD_W21_CS_0 digitalWrite(SS,LOW)   //CS
 #define EPD_W21_CS_1 digitalWrite(SS,HIGH)
 
-
 void SPI_Write(unsigned char value);
 void EPD_W21_WriteDATA(unsigned char datas);
 void EPD_W21_WriteCMD(unsigned char command);
-
-
 
 #endif 
